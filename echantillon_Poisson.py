3286cf49bc07dba9c929a889f2152f237b430b03
Rania Boumahmaza <boumahmaza.rania@gmail.com>
2023-12-14 17:29:05 +0100

    données done !

--- echantillon_Poisson.py
+++ echantillon_Poisson.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def generer_echantillons_poisson(taille, lambda_param):
     """
     Génère des échantillons suivant la loi de Poisson.
@@ -13,3 +14,42 @@ def generer_echantillons_poisson(taille, lambda_param):
     """
     echantillons = np.random.poisson(lambda_param, taille)
     return echantillons
+
+
+def genere_flux(gamma, ß, N, P):
+    """
+    On genere des scénarios de flux avec des lois exponentielles
+    :param gamma: réel positif
+    :param ß: réel positif associé à la moyenne de la loi exp
+    :param N: le nombre de route
+    :param P: le nombre de scénarios
+    :return: le flux
+    """
+    flux = []
+    for j in range(P):
+        x = np.random.exponential(ß, size=N) * gamma
+        flux += x / np.sum(x)
+
+    return flux
+
+def genere_data(N,P,ß,gamma,taille):
+    """
+    On genere des données numériques ainsi que les flux utilisés
+    :param N: le nombre de route
+    :param P: le nombre de scénarios
+    :param ß: réel positif associé à la moyenne de la loi exp
+    :param gamma: réel positif - coef de dominance
+    :param taille: taille de l'échantillon
+    :return: une liste d'observations, une liste de flux
+    """
+    data=[]
+    flux=[]
+    for j in range(P):
+        flux_j=genere_flux(gamma, ß, N, P)
+        data_j=[]
+        for i in flux_j:
+            data_j+=generer_echantillons_poisson(taille,i)
+        data+=data_j
+        flux+=flux_j
+    return data,flux
+
